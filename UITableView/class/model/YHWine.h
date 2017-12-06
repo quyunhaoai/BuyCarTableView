f3b911c12108ce46f8927cb6511c12f6ef98a3dd
quyunhaoai <quyunhaoai@163.com>
2017-12-06 13:10:29 +0800

    f

---
+++ UITableView/class/model/YHWine.h
@@ -0,0 +1,17 @@
+//
+//  YHWine.h
+//  UITableView
+//
+//  Created by hao on 17/9/16.
+//  Copyright © 2017年 hao. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface YHWine : NSObject
+
+@property (nonatomic,strong) NSString *image;
+@property (nonatomic,strong) NSString *money;
+@property (nonatomic,strong) NSString *name;
+@property (nonatomic,assign) NSInteger count;
+@end
---
+++ UITableView/class/view/YHbutton.h
@@ -0,0 +1,13 @@
+//
+//  YHbutton.h
+//  UITableView
+//
+//  Created by hao on 17/9/14.
+//  Copyright © 2017年 hao. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface YHbutton : UIButton
+
+@end
